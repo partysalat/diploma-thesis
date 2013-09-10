c7347102d0c023a66c535f4bf56c543cceff5af5
Ben Lenser <lenser.ben@gmail.com>
2013-09-10 20:00:11 +0200

    Added cpp project

---
+++ code/inverse-problem/src/inverse-problem.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : inverse-problem.cpp
+// Author      : Ben Lenser <lenser.ben@gmail.com>
+// Version     :
+// Copyright   : All Rights reserved
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
